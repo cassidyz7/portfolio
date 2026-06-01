2eed2b434a57747d6d6f16bbfcf8dd3408f648ef
cassidyz7 <cassidyzachary7@gmail.com>
2026-06-01 15:43:10 -0400

    started filling in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,15 @@ import React from "react";
 
 export default function Home() {
     return (
-        <p>Home Page WEEEEEE</p>
+        <div>
+            <div className="intro">
+                <h1>Hi, I'm Zachary Cassidy</h1>
+                <h3>College student aspiring for Software Engineering Co-Op</h3>
+            </div>
+            <div className="boxed">
+                
+            </div>
+        </div>
+        
     )
 }
