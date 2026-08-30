dbb6467419275b4b7dec92f9462b8334014d55f0
cassidyz7 <cassidyzachary7@gmail.com>
2026-08-29 20:23:53 -0400

    finally styling stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import NavbarButton from "./NavbarButton";
 
+import barstyles from "/src/styles/Navbar.module.css";
+import buttonstyles from "/src/styles/NavbarButton.module.css"
+
 export default function Navbar() {
     return (
-        <nav>
-            <NavbarButton to='/portfolio' text='Home' /> | {" "}
-            <NavbarButton to='/about-me' text='About Me'/> | {" "}
-            <NavbarButton to='/projects' text='Projects'/> | {" "}
+        <nav className={barstyles.nav}>
+            <NavbarButton className={buttonstyles.navlink} to='/portfolio' text='Home' />
+            <NavbarButton className={buttonstyles.navlink} to='/about-me' text='About Me'/>
+            <NavbarButton className={buttonstyles.navlink} to='/projects' text='Projects'/>
         </nav>
     );
 }
