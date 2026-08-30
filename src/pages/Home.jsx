18459b9c7149d19b1804c98b84a97426df5e7126
cassidyz7 <cassidyzachary7@gmail.com>
2026-08-29 20:26:46 -0400

    added update message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ export default function Home() {
         <div>
             <h1>Website Under Construction!</h1>
             <p>Check back soon!</p>
+            <p>Last Modification: 8-29-26</p>
+            <p>Began working on styling main page</p>
         </div>
     )
 }
