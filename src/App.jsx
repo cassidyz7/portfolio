650053ba0641b06d202a241281c00f231a9e9d34
cassidyz7 <cassidyzachary7@gmail.com>
2026-09-22 19:58:38 -0400

    once more into the breach

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.DEV ? '/' : '/my-repo-name'}>
             <ScrollToTop/>
             <Navbar/>
             <Routes>
