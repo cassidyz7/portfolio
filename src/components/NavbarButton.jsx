3b9a719763bb4f1c6246debb0c2e500a4c5f7992
cassidyz7 <cassidyzachary7@gmail.com>
2026-09-21 21:48:52 -0400

    Updates

--- src/components/NavbarButton.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export default function NavbarButton({to, text, className}) {
-    return (<Link to={to} className={className}>{text}</Link>)
-}
