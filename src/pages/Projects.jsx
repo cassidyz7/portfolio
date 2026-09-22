a4f7ac73134d334833243fb0c6beecd2db4ab314
cassidyz7 <cassidyzachary7@gmail.com>
2026-09-22 13:31:50 -0400

    added descriptions for half of the projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -18,10 +18,11 @@ export default function Projects() {
             <div className={style.project}>
                 <img src={platform_thumb} className = {style.projPic}></img>
                 <div className={style.projText}>
-                    <h1>This is a title!!!!!</h1>
-                    <h3>This is a really big blob of text describing the thing that is pictured to the left lorem ipsum 
-                        dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
-                        lorem ipsum dolor sit amet lorem ipsum dolor sit amet</h3>
+                    <h1>Custom Platforming Engine</h1>
+                    <h3>Built in C++ using the SDL3 library for graphics, this engine is still a work in progress. This is my longest running project and the one I am the
+                        most proud of. Everything besides the graphics was written from scratch: the physics system, the collision detection, sending keyboard inputs to the player, all of it
+                        hand made. I plan to utilize it in future projects once it is complete.
+                    </h3>
                     <a href="https://github.com/cassidyz7/platformer-v2.0" className={style.projButton} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
                 </div>
             </div>
@@ -29,10 +30,10 @@ export default function Projects() {
             <div className={style.project}>
                 <img src={cpu_thumb} className = {style.projPic}></img>
                 <div className={style.projText}>
-                    <h1>This is a title!!!!!</h1>
-                    <h3>This is a really big blob of text describing the thing that is pictured to the left lorem ipsum 
-                        dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
-                        lorem ipsum dolor sit amet lorem ipsum dolor sit amet</h3>
+                    <h1>Simple Assembly and CPU</h1>
+                    <h3>This was created to become more familiar with the process of designing and implementing a CPU based on specifications for an assembly language. 
+                        The assembly language used was also created for this, as I wanted a simplified and more stripped down version of MIPS, which was being used in my CSCI 250: Concepts of Computer Systems course. 
+                        This was a way for me to both work on an interesting and fun project while also studying for the exam on this material.</h3>
                     <a href="https://github.com/cassidyz7/Assembly-and-CPU" className={style.projButton} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
                 </div>
             </div>
@@ -40,7 +41,7 @@ export default function Projects() {
             <div className={style.project}>
                 <img src={thumb_3d} className = {style.projPic}></img>
                 <div className={style.projText}>
-                    <h1>This is a title!!!!!</h1>
+                    <h1>Basic 3D Renderer</h1>
                     <h3>This is a really big blob of text describing the thing that is pictured to the left lorem ipsum 
                         dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
                         lorem ipsum dolor sit amet lorem ipsum dolor sit amet</h3>
@@ -51,7 +52,7 @@ export default function Projects() {
             <div className={style.project}>
                 <img src={background_port} className = {style.projPic}></img>
                 <div className={style.projText}>
-                    <h1>This is a title!!!!!</h1>
+                    <h1>Portfolio Website</h1>
                     <h3>This is a really big blob of text describing the thing that is pictured to the left lorem ipsum 
                         dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
                         lorem ipsum dolor sit amet lorem ipsum dolor sit amet</h3>
