ea6f63983490239be75f761df59f711fd3e496c2
cassidyz7 <cassidyzachary7@gmail.com>
2026-09-22 19:51:10 -0400

    ok i think i got it

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter>
             <ScrollToTop/>
             <Navbar/>
             <Routes>
