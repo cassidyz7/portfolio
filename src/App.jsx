6f4be68460e667296802f85a4d99639c671b6563
cassidyz7 <cassidyzachary7@gmail.com>
2026-09-22 20:04:14 -0400

    undo

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
     return (
-        <BrowserRouter basename={import.meta.env.DEV ? '/' : '/my-repo-name'}>
+        <BrowserRouter>
             <ScrollToTop/>
             <Navbar/>
             <Routes>
